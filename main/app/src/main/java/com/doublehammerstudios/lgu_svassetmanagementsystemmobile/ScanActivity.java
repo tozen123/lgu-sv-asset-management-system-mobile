2040ea25b2f23cc11f9e5eb03e8d63bb039e905e
Kirby Ann De Los Reyes <delosreyeskirbyann@gmail.com>
2023-11-27 16:49:32 +0800

    new

--- main/app/src/main/java/com/doublehammerstudios/lgu_svassetmanagementsystemmobile/ScanActivity.java
+++ main/app/src/main/java/com/doublehammerstudios/lgu_svassetmanagementsystemmobile/ScanActivity.java
@@ -1,14 +1,176 @@
 package com.doublehammerstudios.lgu_svassetmanagementsystemmobile;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.util.Collections;
 
 public class ScanActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private TextureView textureViewCamera;
+    private CameraDevice cameraDevice;
+    private CaptureRequest.Builder previewRequestBuilder;
+    private CameraCaptureSession cameraCaptureSession;
+
+    private final TextureView.SurfaceTextureListener surfaceTextureListener =
+            new TextureView.SurfaceTextureListener() {
+                @Override
+                public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+                    openCamera();
+                }
+
+                @Override
+                public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+                }
+
+                @Override
+                public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                    return false;
+                }
+
+                @Override
+                public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        textureViewCamera = findViewById(R.id.textureViewCamera);
+        textureViewCamera.setSurfaceTextureListener(surfaceTextureListener);
+    }
+
+    private void openCamera() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        String cameraId = null;
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                return;
+            }
+            cameraId = cameraManager.getCameraIdList()[0]; // Get the first camera
+            cameraManager.openCamera(cameraId, new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(@NonNull CameraDevice cameraDevice) {
+                    ScanActivity.this.cameraDevice = cameraDevice;
+                    createCameraPreviewSession();
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+                    cameraDevice.close();
+                    ScanActivity.this.cameraDevice = null;
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice cameraDevice, int error) {
+                    cameraDevice.close();
+                    ScanActivity.this.cameraDevice = null;
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createCameraPreviewSession() {
+        try {
+            SurfaceTexture texture = textureViewCamera.getSurfaceTexture();
+            assert texture != null;
+            texture.setDefaultBufferSize(1920, 1080);
+
+            Surface surface = new Surface(texture);
+            previewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            previewRequestBuilder.addTarget(surface);
+
+            cameraDevice.createCaptureSession(Collections.singletonList(surface),
+                    new CameraCaptureSession.StateCallback() {
+                        @Override
+                        public void onConfigured(@NonNull CameraCaptureSession session) {
+                            if (cameraDevice == null) {
+                                return;
+                            }
+                            cameraCaptureSession = session;
+                            updatePreview();
+                        }
+
+                        @Override
+                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                        }
+                    }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updatePreview() {
+        if (cameraDevice == null) {
+            return;
+        }
+        previewRequestBuilder.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
+        try {
+            cameraCaptureSession.setRepeatingRequest(previewRequestBuilder.build(), null, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (textureViewCamera.isAvailable()) {
+            openCamera();
+        } else {
+            textureViewCamera.setSurfaceTextureListener(surfaceTextureListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        closeCamera();
+        super.onPause();
+    }
+
+    private void closeCamera() {
+        if (cameraDevice != null) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+        if (cameraCaptureSession != null) {
+            cameraCaptureSession.close();
+            cameraCaptureSession = null;
+        }
     }
 }
--- main/app/src/main/java/com/doublehammerstudios/lgu_svassetmanagementsystemmobile/SecondActivity.java
+++ main/app/src/main/java/com/doublehammerstudios/lgu_svassetmanagementsystemmobile/SecondActivity.java
@@ -13,7 +13,7 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Button startButton = findViewById(R.id.startButton);
+        Button startButton = findViewById(R.id.scanButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

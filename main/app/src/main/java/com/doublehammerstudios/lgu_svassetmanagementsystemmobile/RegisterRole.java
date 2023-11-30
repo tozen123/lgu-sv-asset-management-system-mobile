070535caad2007ef3789c61b36080fbdb10abb4f
Kirby Ann De Los Reyes <delosreyeskirbyann@gmail.com>
2023-12-01 02:04:48 +0800

    new

---
+++ main/app/src/main/java/com/doublehammerstudios/lgu_svassetmanagementsystemmobile/RegisterRole.java
@@ -0,0 +1,14 @@
+package com.doublehammerstudios.lgu_svassetmanagementsystemmobile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterRole extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_role);
+    }
+}

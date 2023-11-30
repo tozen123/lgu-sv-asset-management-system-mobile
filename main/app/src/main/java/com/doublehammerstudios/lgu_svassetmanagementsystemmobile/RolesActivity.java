b62cad97ee0bfcb038ee0531012d53f6076d1e84
Kirby Ann De Los Reyes <delosreyeskirbyann@gmail.com>
2023-11-30 22:22:52 +0800

    new

---
+++ main/app/src/main/java/com/doublehammerstudios/lgu_svassetmanagementsystemmobile/RolesActivity.java
@@ -0,0 +1,14 @@
+package com.doublehammerstudios.lgu_svassetmanagementsystemmobile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RolesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.roles_activity);
+    }
+}

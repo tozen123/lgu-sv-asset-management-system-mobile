0f77440b762d03f98403c9b05a1f4563fea117b1
Kirby Ann De Los Reyes <delosreyeskirbyann@gmail.com>
2023-12-01 02:39:41 +0800

    new

---
+++ main/app/src/main/java/com/doublehammerstudios/lgu_svassetmanagementsystemmobile/AccountSetupActivity.java
@@ -0,0 +1,14 @@
+package com.doublehammerstudios.lgu_svassetmanagementsystemmobile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_setup_activity);
+    }
+}
